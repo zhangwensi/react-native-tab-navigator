02c346b5c76050894cf84b5e9bbc171c72dfc2ee
unknown <1248018887@qq.com>
2022-11-10 10:55:59 +0800

    自建插件

---
+++ config/ViewPropTypes.js
@@ -0,0 +1,6 @@
+import { View } from 'react-native'
+import { ViewPropTypes as RNViewPropTypes } from 'deprecated-react-native-prop-types'
+
+const ViewPropTypes = RNViewPropTypes || View.propTypes
+
+export default ViewPropTypes
